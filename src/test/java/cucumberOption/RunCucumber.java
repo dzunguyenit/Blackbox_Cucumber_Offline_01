1a2a6e622296ddb85d3083490f317644cc4793ee
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-10-21 09:38:50 +0700

    up

--- src/test/java/cucumberOption/RunCucumber.java
+++ src/test/java/cucumberOption/RunCucumber.java
@@ -11,6 +11,9 @@ import cucumber.api.junit.Cucumber;
 		"html:target/site/cucumber-reports",
 		"json:target/site/cucumber.json" }, snippets = SnippetType.CAMELCASE, tags = {
 				"@LogInGuruBank,@CreateCustomer,@EditCustomer" })
+// mvn install
+// mvn -Dbrowser=chrome -Dversion=76.0.3809.68 -Durl=http://demo.guru99.com/v4/ test verify
+//mvn -Dbrowser=firefox -Dversion=0.19.0 -Durl=http://demo.guru99.com/v4/ test verify
 public class RunCucumber {
 
 }
